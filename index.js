89f439c10dab473b057bc44af8fb9d3c990acb4d
Abhi RAW <abhi@example.com>
2025-02-21 21:08:53 +0530

    2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-      origin: ["http://localhost:5173","https://ecom-eta-three.vercel.app"],
+      origin: ["http://localhost:5173","https://ecom-eta-three.vercel.app","https://client-100.vercel.app"],
       methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
       credentials: true,
   })
